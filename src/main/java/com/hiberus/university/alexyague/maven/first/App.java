78218b4ee0deae268857f0c5feb0d4dfc6a51720
ayague <ayague@hiberus.com>
2023-04-18 18:18:02 +0200

    Commit inicial con programa de prueba y el pom.xml

---
+++ src/main/java/com/hiberus/university/alexyague/maven/first/App.java
@@ -0,0 +1,13 @@
+package com.hiberus.university.alexyague.maven.first;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
